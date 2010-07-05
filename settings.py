07d81ed74b3dedb8143319436416508999d74901
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2010-07-05 17:48:19 +0800

    Syncing up additional settings from bookworm's original settings.py to our new one

--- settings.py
+++ settings.py
@@ -2,14 +2,19 @@
 
 from local import * # Local installation settings in local.py
 import os
+import logging, logging.handlers
 
+# Live site settings (others should override in local.py)
 ROOT_PATH = os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+TESTING = False # Override this in test_settings.py
+
+BASE_URL = ''
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+     ('Ruiwen Chua', 'ruiwen+socialbooks@thoughtmonkeys.com'),
 )
 
 MANAGERS = ADMINS
@@ -73,11 +78,25 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'socialbooks.middleware.Language',    
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django_authopenid.middleware.OpenIDMiddleware',
+    'django.middleware.http.SetRemoteAddrFromForwardedFor',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+    'socialbooks.minidetector.Middleware',
+    'socialbooks.middleware.Mobile',
+    'socialbooks.api.middleware.SSLRedirect',
+    'socialbooks.api.middleware.APIKeyCheck',
+
 )
 
 ROOT_URLCONF = 'socialbooks.urls'
@@ -102,6 +121,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'django.contrib.flatpages',
+    'django.contrib.sitemaps',
+    'django.contrib.humanize',
+    'django_authopenid',
     'south',
     'socialbooks.minidetector',
     'socialbooks.library',
@@ -110,3 +133,116 @@ INSTALLED_APPS = (
     'socialbooks.mobile',
     'socialbooks.api',
 )
+
+AUTH_PROFILE_MODULE = "library.userpref"
+
+ugettext = lambda s: s
+LOGIN_URL = '/%s%s%s' %  (BASE_URL, ugettext('account/'), ugettext('signin/'))
+SITE_ID = 2
+
+DEFAULT_NUM_RESULTS = 20
+DEFAULT_START_PAGE = 1
+DEFAULT_ORDER_FIELD = 'created_time'
+DEFAULT_ORDER_DIRECTION = 'desc'
+VALID_ORDER_DIRECTIONS = ('asc', 'desc')
+VALID_ORDER_FIELDS = ('created_time', 'title', 'orderable_author')
+
+# Search database info
+SEARCH_ROOT = os.path.join(ROOT_PATH, 'search')
+
+# Are we running with mobile settings on?
+MOBILE = False
+FORCE_SCRIPT_NAME = ''
+
+# Domain which to redirect requests that are coming from a mobile device
+MOBILE_HOST = 'http://m.bookworm.oreilly.com/'
+
+# Hosting credit
+HOSTING_CREDIT = "O'Reilly Media"
+HOSTING_CREDIT_URL = 'http://oreilly.com/'
+
+# Email reply-to address
+REPLYTO_EMAIL = 'donotreply@oreilly.com'
+DEFAULT_FROM_EMAIL = REPLYTO_EMAIL
+
+# The admin address that's displayed on the site in help pages
+DISPLAY_ADMIN_EMAIL = 'bookworm@oreilly.com'
+ADMIN_EMAIL = DISPLAY_ADMIN_EMAIL
+
+# Set up logging
+LOG_DIR = '%s/log/' % ROOT_PATH
+LOG_NAME = 'socialbooks.log'
+
+TEST_DATABASE_CHARSET = 'utf8'
+TEST_DATABASE_COLLATION='utf8_unicode_ci'
+
+SEARCH_ROOT = os.path.join(ROOT_PATH, 'search', 'dbs')
+
+CACHE_BACKEND = 'file:///tmp/socialbooks/django_cache'
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+CACHE_TEMPLATE_TIMEOUT = 60 * 60 * 1
+
+DATE_FORMAT = "l, N j Y"
+LIBRARYTHING_KEY = ''
+
+# Access time, filename/function#line-number message
+log_formatter = logging.Formatter("[%(asctime)s %(filename)s/%(funcName)s#%(lineno)d] %(message)s")
+
+# This should roll logs over at midnight and date-stamp them appropriately
+handler = logging.handlers.TimedRotatingFileHandler(filename="%s/%s" % (LOG_DIR, LOG_NAME),
+                                                        when='midnight')
+handler.setFormatter(log_formatter)
+log = logging.getLogger('')
+log.setLevel(logging.INFO)
+log.addHandler(handler)
+
+# If set, the templates will load jQuery locally instead of from Google
+OFFLINE = False
+
+# Google Analytics key
+ANALYTICS_KEY = ''
+
+# The email addresses of the users who should receive an error email 
+# (should be a list)
+ERROR_EMAIL_RECIPIENTS = (ADMINS[0][1], )
+
+# The URL for the epubcheck webservice
+EPUBCHECK_WEBSERVICE = 'http://threepress.org/epubcheck-service/' 
+
+# Apps to test
+TEST_APPS = ('library',)
+
+TESTING = False
+
+# Feedbooks OPDS feed
+FEEDBOOKS_OPDS_FEED = 'http://feedbooks.com/books/top.atom'
+
+# Always upload files to the filesystem
+FILE_UPLOAD_HANDLERS = ("django.core.files.uploadhandler.TemporaryFileUploadHandler",)
+
+# Maximum number of CSS files to attempt to display at once
+MAX_CSS_FILES = 10
+
+# API key field name
+API_FIELD_NAME = 'api_key'
+
+# Hostname (no trailing slash)
+HOSTNAME = 'http://ubuntuvm:8080'
+
+# Secure hostname (no trailing slash)
+SECURE_HOSTNAME = 'https://ubuntuvm:8080'
+
+XSLT_DIR = os.path.join(ROOT_PATH, 'library', 'xsl')
+DTBOOK2XHTML = os.path.join(XSLT_DIR, 'dtbook2xhtml.xsl')
+
+# Don't ever try to call epubcheck -- useful in testing offline
+SKIP_EPUBCHECK = False
+
+CUSTOMER_SERVICE_URL = 'http://getsatisfaction.com/oreilly'
+CUSTOMER_SERVICE_NAME = 'Get Satisfaction'
+  
+try:
+    from local import *
+except ImportError:
+    pass
+ 
