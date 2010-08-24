d3a3a8ee0cafb2cd3afc56a8e190a2e2e936f6b3
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2010-08-24 20:08:23 +0800

    Using from socialbooks.library.models import X instead of importing models as a module

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import never_cache
 from django.views.generic.simple import direct_to_template
 from django.core.urlresolvers import reverse
 
-from socialbooks.library import models 
+from socialbooks.library.models import EpubArchive
 from socialbooks.library.views import download_epub, add_by_url_field, add_data_to_document, download_jsbook
 from socialbooks.api import HttpResponseCreated, SocialbooksHttpResponseNotAcceptable 
 from socialbooks.api.forms import APIUploadForm
@@ -18,7 +18,7 @@ import json
 def library(request, select=None):
 	
 	if request.method == 'GET':
-		documents = models.EpubArchive.objects.only('id', 'title', 'orderable_author').filter(user_archive__user=request.user).order_by(settings.DEFAULT_ORDER_FIELD).distinct()
+		documents = EpubArchive.objects.only('id', 'title', 'orderable_author').filter(user_archive__user=request.user).order_by(settings.DEFAULT_ORDER_FIELD).distinct()
 
 		out = []
 		if select is None:
@@ -59,7 +59,7 @@ def api_book(request, title, key, nonce=None):
 @login_required
 def api_book_meta(request, key):
 	if request.method == 'GET':
-		document = models.EpubArchive.objects.only('id', 'title', 'orderable_author').get(id=key)
+		document = EpubArchive.objects.only('id', 'title', 'orderable_author').get(id=key)
 		
 		if document is not None:
 			out = {"id":document.id, "title": document.title, "author": document.orderable_author}
@@ -69,13 +69,14 @@ def api_book_meta(request, key):
 		else:
 			raise Http404
 
+
 @never_cache
 @login_required
 def main(request, SSL=True):
     '''Main entry point; dispatch by request type for upload vs. GET operations'''
     if request.method == 'GET':
         # No-arg GET request is a library listing; return all the user's documents
-        documents = models.EpubArchive.objects.filter(user_archive__user=request.user).order_by(settings.DEFAULT_ORDER_FIELD).distinct()
+        documents = EpubArchive.objects.filter(user_archive__user=request.user).order_by(settings.DEFAULT_ORDER_FIELD).distinct()
         return direct_to_template(request, 'api/list.html', 
                                   {'documents': documents})
 
@@ -90,7 +91,7 @@ def main(request, SSL=True):
             if form.is_valid():
                 temp_file = request.FILES['epub_data'].temporary_file_path()
                 document_name = form.cleaned_data['epub_data'].name
-                document = models.EpubArchive.objects.create(name=document_name)
+                document = EpubArchive.objects.create(name=document_name)
                 document.save()
                 resp = add_data_to_document(request, document, open(temp_file), form, redirect_success_to_page=False)
                 
@@ -99,7 +100,7 @@ def main(request, SSL=True):
         else:
             return SocialbooksHttpResponseNotAcceptable("You must either include epub_url or epub_data in your request")
 
-        if isinstance(resp, models.EpubArchive):
+        if isinstance(resp, EpubArchive):
             # This was a successful add and we got back a document
             return HttpResponseCreated("%s%s" % (settings.SECURE_HOSTNAME, reverse('api_download', args=[resp.id])))
 
